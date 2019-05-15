9d59cdfd0eb0f05235b0869044c126eaf789140d
Gaurav Ramtri <gaurav.ramtri2017@vitstudent.ac.in>
2019-05-16 03:43:30 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'internship';
+  items: Array<any> = []
+  
+
+  constructor() {
+    this.items = [
+      { name: 'assets/images/thumb1.png' },
+      { name: 'assets/images/thumb2.png' },
+      { name: 'assets/images/thumb3.png' },
+      { name: 'assets/images/thumb4.png' },
+      { name: 'assets/images/thumb5.png' },
+      { name: 'assets/images/thumb6.png' },
+      { name: 'assets/images/thumb1.png' },
+      { name: 'assets/images/thumb2.png' },
+      { name: 'assets/images/thumb3.png' },
+      { name: 'assets/images/thumb4.png' },
+      { name: 'assets/images/thumb5.png' },
+      { name: 'assets/images/thumb6.png' },
+
+
+    ]
+  }
+  latitude = 28.4721;
+  longitude = 77.0726;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {AgmCoreModule} from '@agm/core';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { Ng2CarouselamosModule } from 'ng2-carouselamos';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    
     BrowserModule,
-    AppRoutingModule
+    Ng2CarouselamosModule,
+    AgmCoreModule.forRoot({
+      apiKey:'AIzaSyAc0SGkFq92IcqQqLN_Kec_PrzXPZ9iKZ8'
+    }),
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
