839e2f6fa9e5a534a0c131b4e92e6d5fb2bad9c0
Gaurav Ramtri <gaurav.ramtri2017@vitstudent.ac.in>
2019-05-17 23:49:29 +0530

    Pipe search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-
+import {FilterPipe} from './filter.pipe'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  title:String;
+  names:any;
   items: Array<any> = []
   
 
@@ -23,9 +25,19 @@ export class AppComponent {
       { name: 'assets/images/thumb4.png' },
       { name: 'assets/images/thumb5.png' },
       { name: 'assets/images/thumb6.png' },
+      { name: 'assets/images/thumb1.png' },
+      { name: 'assets/images/thumb2.png' },
+      { name: 'assets/images/thumb3.png' },
+      { name: 'assets/images/thumb4.png' },
+      { name: 'assets/images/thumb5.png' },
+      { name: 'assets/images/thumb6.png' },
+      
+
 
 
     ]
+    this.title="Angular search";
+      this.names=['Gaurav','Anukriti','Sunita','Dharmender','Divyanshu','Ishita','Sarthak','karan']
   }
   latitude = 28.4721;
   longitude = 77.0726;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+import { AppComponent } from './app.component';
+import { FilterPipe }from './filter.pipe';
 import {AgmCoreModule} from '@agm/core';
 
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
 import { Ng2CarouselamosModule } from 'ng2-carouselamos';
+
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+
+
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    
+    AppComponent, FilterPipe
+    
+    
+    
+    
   ],
   imports: [
     
     BrowserModule,
+    FormsModule,
+    
     Ng2CarouselamosModule,
     AgmCoreModule.forRoot({
       apiKey:'AIzaSyAc0SGkFq92IcqQqLN_Kec_PrzXPZ9iKZ8'
-    }),
+    })
+    
     
   ],
   providers: [],
